cf59cdf80ba24169cbb160944b3551828145b74f
heewonJ1230 <34181260+heewonJ1230@users.noreply.github.com>
2024-03-20 12:37:26 +0900

    Update index.js

--- fakediefie/index.js
+++ fakediefie/index.js
@@ -7,11 +7,21 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
+// Import the functions you need from the SDKs you need
+const functions = require('firebase-functions');
+// ... 여기에 여치니의 다른 import 문장들이 있을 수 있어요 ...
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// OpenAI API 키를 가져옵니다.
+const OPENAI_API_KEY = functions.config().openai.key;
+
+// 여기에 여치니의 함수 코드들이 있을 거예요
+exports.yourFunction = functions.https.onRequest((request, response) => {
+});
+// 여기에 요청을 처리하는 코드... OPENAI_API_KEY 변수를 사용합니다;
+
+// Create and deploy your first functions;
+// https://firebase.google.com/docs/functions/get-started;
 
 // exports.helloWorld = onRequest((request, response) => {
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
-// });
